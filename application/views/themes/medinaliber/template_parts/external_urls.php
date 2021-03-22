4481d2c27df17871d740e736d5cd2393db14dc94
sosaheri <sosaheriberto2001@gmail.com>
2021-03-22 17:07:50 -0400

    feat: add urls.

--- application/views/themes/medinaliber/template_parts/external_urls.php
+++ application/views/themes/medinaliber/template_parts/external_urls.php
@@ -11,34 +11,34 @@
 						
 							<div class="divTableRow">
 							<div class="divTableCell" style="width:150px;"></div>
-							<div class="divTableCell"><a href="#">Request A New Book</a></div>
-							<div class="divTableCell"><a href="#">My Publishing Proposals</a></div>
-							<div class="divTableCell"><a href="#">Contact with Us</a></div>
+							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('Request_A_New_Book'); ?></a></div>
+							<div class="divTableCell"><a target="_blank" href="../clients/proposals"><?php echo _l('My_Publishing_Proposals'); ?></a></div>
+							<div class="divTableCell"><a target="_blank" href="../clients/open_ticket"><?php echo _l('Contact_with_Us'); ?></a></div>
 							<div class="divTableCell"></div>
 							</div>
 
 							<div class="divTableRow">
 							<div class="divTableCell"></div>
-							<div class="divTableCell"><a href="#">Printing Order</a></div>
-							<div class="divTableCell"><a href="#">My Services Budgets</a></div>
-							<div class="divTableCell"><a href="#">My Tickets</a></div>
+							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('Printing_Order'); ?></a></div>
+							<div class="divTableCell"><a target="_blank"href="../clients/estimates"><?php echo _l('My_Services_Budgets'); ?></a></div>
+							<div class="divTableCell"><a target="_blank" href="../clients/tickets"><?php echo _l('My_Tickets'); ?></a></div>
 							<div class="divTableCell"></div>
 							</div>
 
 							
 							<div class="divTableRow">
 							<div class="divTableCell"></div>
-							<div class="divTableCell"><a href="#">Promote My Book</a></div>
-							<div class="divTableCell"><a href="#">My Invoices</a></div>
-							<div class="divTableCell"><a href="#">All Royalties</a></div>
+							<div class="divTableCell"><a target="_blank" href="https://verkuo.medinaliber.com/products/client"><?php echo _l('Promote_My_Book'); ?></a></div>
+							<div class="divTableCell"><a target="_blank" href="../clients/invoices"><?php echo _l('My_Invoices'); ?></a></div>
+							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('All_Royalties'); ?></a></div>
 							<div class="divTableCell"></div>
 							</div>
 							
 							<div class="divTableRow">
 							<div class="divTableCell"></div>								
-							<div class="divTableCell"><a href="#">Sale Channels</a></div>
-							<div class="divTableCell"><a href="#">My Author's Profile</a></div>
-							<div class="divTableCell"><a href="#">Request Payment</a></div>
+							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('Sale_Channels'); ?></a></div>
+							<div class="divTableCell"><a target="_blank" href="../clients/profile"><?php echo _l('My_Authors_Profile'); ?></a></div>
+							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('Request_Payment'); ?></a></div>
 							<div class="divTableCell"></div>
 						</div>
 						</div>
