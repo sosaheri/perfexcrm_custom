1fac7cb98abfa6a3d784212df10307339b77737c
sosaheri <sosaheriberto2001@gmail.com>
2021-03-23 09:39:00 -0400

    feat: add responsive external urls.

--- application/views/themes/medinaliber/template_parts/external_urls.php
+++ application/views/themes/medinaliber/template_parts/external_urls.php
@@ -1,49 +1,41 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
-<div id="ExternalUrl" class="panel_s">
-				<div id="ExternalUrl" class="panel-body">
-					<h3 class="text-success projects-summary-heading no-mtop mbot15"></h3>
-					<div class="row">
-					
-					<div class="divTable ExternalUrl">
-						<div class="divTableBody">
 
-						
-							<div class="divTableRow">
-							<div class="divTableCell" style="width:150px;"></div>
-							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('Request_A_New_Book'); ?></a></div>
-							<div class="divTableCell"><a target="_blank" href="../clients/proposals"><?php echo _l('My_Publishing_Proposals'); ?></a></div>
-							<div class="divTableCell"><a target="_blank" href="../clients/open_ticket"><?php echo _l('Contact_with_Us'); ?></a></div>
-							<div class="divTableCell"></div>
-							</div>
+<div class='rg-container'>
+	<table class='rg-table'>
+				<tbody>
+			
+				<tr class=''>
+					<td class='text '><a target="_blank" href="#"><?php echo _l('Request_A_New_Book'); ?></a></td>
+					<td class='text '><a target="_blank" href="../clients/proposals"><?php echo _l('My_Publishing_Proposals'); ?></a></td>
+					<td class='text '><a target="_blank" href="../clients/open_ticket"><?php echo _l('Contact_with_Us'); ?></a></td>
+				</tr>
+			
 
-							<div class="divTableRow">
-							<div class="divTableCell"></div>
-							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('Printing_Order'); ?></a></div>
-							<div class="divTableCell"><a target="_blank"href="../clients/estimates"><?php echo _l('My_Services_Budgets'); ?></a></div>
-							<div class="divTableCell"><a target="_blank" href="../clients/tickets"><?php echo _l('My_Tickets'); ?></a></div>
-							<div class="divTableCell"></div>
-							</div>
+				<tr class=''>
+					<td class='text '><a target="_blank" href="#"><?php echo _l('Printing_Order'); ?></a></td>
+					<td class='text '><a target="_blank"href="../clients/estimates"><?php echo _l('My_Services_Budgets'); ?></a></td>
+					<td class='text '><a target="_blank" href="../clients/tickets"><?php echo _l('My_Tickets'); ?></a></td>
+				</tr>
+			
 
-							
-							<div class="divTableRow">
-							<div class="divTableCell"></div>
-							<div class="divTableCell"><a target="_blank" href="https://verkuo.medinaliber.com/products/client"><?php echo _l('Promote_My_Book'); ?></a></div>
-							<div class="divTableCell"><a target="_blank" href="../clients/invoices"><?php echo _l('My_Invoices'); ?></a></div>
-							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('All_Royalties'); ?></a></div>
-							<div class="divTableCell"></div>
-							</div>
-							
-							<div class="divTableRow">
-							<div class="divTableCell"></div>								
-							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('Sale_Channels'); ?></a></div>
-							<div class="divTableCell"><a target="_blank" href="../clients/profile"><?php echo _l('My_Authors_Profile'); ?></a></div>
-							<div class="divTableCell"><a target="_blank" href="#"><?php echo _l('Request_Payment'); ?></a></div>
-							<div class="divTableCell"></div>
-						</div>
-						</div>
-					</div>
+				<tr class=''>
+					<td class='text '><a target="_blank" href="https://verkuo.medinaliber.com/products/client"><?php echo _l('Promote_My_Book'); ?></a></td>
+					<td class='text '><a target="_blank" href="../clients/invoices"><?php echo _l('My_Invoices'); ?></a></td>
+					<td class='text '><a target="_blank" href="#"><?php echo _l('All_Royalties'); ?></a></td>
+				</tr>
+			
 
-					</div>
-				</div>
-			</div>
+				<tr class=''>
+					<td class='text '><a target="_blank" href="#"><?php echo _l('Sale_Channels'); ?></a></td>
+					<td class='text '><a target="_blank" href="../clients/profile"><?php echo _l('My_Authors_Profile'); ?></a></td>
+					<td class='text '><a target="_blank" href="#"><?php echo _l('Request_Payment'); ?></a></td>
+				</tr>
+			
+		</tbody>
+	</table>
+</div>
+
+
+
+
