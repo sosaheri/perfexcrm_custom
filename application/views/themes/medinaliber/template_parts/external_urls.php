70bd2701eaf4571313d84fdbdb23902a7fb34a09
sosaheri <sosaheriberto2001@gmail.com>
2021-03-23 18:02:00 -0400

    feat: add new external url.

--- application/views/themes/medinaliber/template_parts/external_urls.php
+++ application/views/themes/medinaliber/template_parts/external_urls.php
@@ -7,28 +7,28 @@
 			
 				<tr class=''>
 					<td class='text '><a target="_blank" href="#"><?php echo _l('Request_A_New_Book'); ?></a></td>
-					<td class='text '><a target="_blank" href="../clients/proposals"><?php echo _l('My_Publishing_Proposals'); ?></a></td>
-					<td class='text '><a target="_blank" href="../clients/open_ticket"><?php echo _l('Contact_with_Us'); ?></a></td>
+					<td class='text '><a href="../clients/proposals"><?php echo _l('My_Publishing_Proposals'); ?></a></td>
+					<td class='text '><a href="../clients/open_ticket"><?php echo _l('Contact_with_Us'); ?></a></td>
 				</tr>
 			
 
 				<tr class=''>
 					<td class='text '><a target="_blank" href="#"><?php echo _l('Printing_Order'); ?></a></td>
-					<td class='text '><a target="_blank"href="../clients/estimates"><?php echo _l('My_Services_Budgets'); ?></a></td>
-					<td class='text '><a target="_blank" href="../clients/tickets"><?php echo _l('My_Tickets'); ?></a></td>
+					<td class='text '><a href="../clients/estimates"><?php echo _l('My_Services_Budgets'); ?></a></td>
+					<td class='text '><a href="../clients/tickets"><?php echo _l('My_Tickets'); ?></a></td>
 				</tr>
 			
 
 				<tr class=''>
 					<td class='text '><a target="_blank" href="https://verkuo.medinaliber.com/products/client"><?php echo _l('Promote_My_Book'); ?></a></td>
-					<td class='text '><a target="_blank" href="../clients/invoices"><?php echo _l('My_Invoices'); ?></a></td>
-					<td class='text '><a target="_blank" href="#"><?php echo _l('All_Royalties'); ?></a></td>
+					<td class='text '><a href="../clients/invoices"><?php echo _l('My_Invoices'); ?></a></td>
+					<td class='text '><a href="../clients/files"><?php echo _l('All_Royalties'); ?></a></td>
 				</tr>
 			
 
 				<tr class=''>
 					<td class='text '><a target="_blank" href="#"><?php echo _l('Sale_Channels'); ?></a></td>
-					<td class='text '><a target="_blank" href="../clients/profile"><?php echo _l('My_Authors_Profile'); ?></a></td>
+					<td class='text '><a href="../clients/profile"><?php echo _l('My_Authors_Profile'); ?></a></td>
 					<td class='text '><a target="_blank" href="#"><?php echo _l('Request_Payment'); ?></a></td>
 				</tr>
 			
