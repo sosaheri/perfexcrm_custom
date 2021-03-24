45dbde62c5a6aaee5a513aff0ddc495ac52725ce
sosaheri <sosaheriberto2001@gmail.com>
2021-03-24 12:23:29 -0400

    feat: add news external urls.

--- application/views/themes/medinaliber/template_parts/external_urls.php
+++ application/views/themes/medinaliber/template_parts/external_urls.php
@@ -6,30 +6,30 @@
 				<tbody>
 			
 				<tr class=''>
-					<td class='text '><a target="_blank" href="#"><?php echo _l('Request_A_New_Book'); ?></a></td>
+					<td class='text '><a href="../clients/form_request_a_book"><?php echo _l('Request_A_New_Book'); ?></a></td>
 					<td class='text '><a href="../clients/proposals"><?php echo _l('My_Publishing_Proposals'); ?></a></td>
 					<td class='text '><a href="../clients/open_ticket"><?php echo _l('Contact_with_Us'); ?></a></td>
 				</tr>
 			
 
 				<tr class=''>
-					<td class='text '><a target="_blank" href="#"><?php echo _l('Printing_Order'); ?></a></td>
+					<td class='text '><a href="../clients/form_printing_order"><?php echo _l('Printing_Order'); ?></a></td>
 					<td class='text '><a href="../clients/estimates"><?php echo _l('My_Services_Budgets'); ?></a></td>
 					<td class='text '><a href="../clients/tickets"><?php echo _l('My_Tickets'); ?></a></td>
 				</tr>
 			
 
 				<tr class=''>
-					<td class='text '><a target="_blank" href="https://verkuo.medinaliber.com/products/client"><?php echo _l('Promote_My_Book'); ?></a></td>
+					<td class='text '><a href="../products/client"><?php echo _l('Promote_My_Book'); ?></a></td>
 					<td class='text '><a href="../clients/invoices"><?php echo _l('My_Invoices'); ?></a></td>
 					<td class='text '><a href="../clients/files"><?php echo _l('All_Royalties'); ?></a></td>
 				</tr>
 			
 
 				<tr class=''>
-					<td class='text '><a target="_blank" href="#"><?php echo _l('Sale_Channels'); ?></a></td>
+					<td class='text '><a href="../clients/sales_channel"><?php echo _l('Sale_Channels'); ?></a></td>
 					<td class='text '><a href="../clients/profile"><?php echo _l('My_Authors_Profile'); ?></a></td>
-					<td class='text '><a target="_blank" href="#"><?php echo _l('Request_Payment'); ?></a></td>
+					<td class='text '><a href="../clients/form_request_payment"><?php echo _l('Request_Payment'); ?></a></td>
 				</tr>
 			
 		</tbody>
